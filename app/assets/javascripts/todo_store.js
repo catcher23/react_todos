00ff2dc51b7b7768becef066d20a4dbe78bf711a
catcher1 <danlau168@gmail.com>
2015-10-06 21:53:35 -0700

    Finish phase 1

---
+++ app/assets/javascripts/todo_store.js
@@ -0,0 +1,92 @@
+(function (root) {
+  var _todos = [], _callbacks = [];
+
+  root.TodoStore = {
+    changed: function(){
+      _callbacks.forEach(function(cb){ cb(); });
+    },
+
+    addChangedHandler: function(callback){
+      _callbacks.push(callback);
+    },
+
+    removeChangedHandler: function(callback){
+      _callbacks.splice(_callbacks.indexof(callback), 1);
+    },
+
+    fetch: function() {
+      var that = this;
+      $.ajax({
+        method: 'GET',
+        url: 'api/todos',
+        dataType: 'json',
+        success: function(resp) {
+          _todos = resp;
+          that.changed();
+        }
+      });
+    },
+
+    create: function(object) {
+      var that = this;
+      $.ajax({
+        method: 'POST',
+        url: 'api/todos',
+        data: {todo: object},
+        success: function(resp) {
+          _todos.push(resp);
+          that.changed();
+        }
+      });
+    },
+
+    find: function(id) {
+      var idx = -1;
+      for (var i = 0; i < _todos.length; i++) {
+        if (_todos[i].id === id) {
+          idx = i;
+          break;
+        }
+      }
+
+      return idx;
+    },
+
+    destroy: function(id) {
+      var that = this, idx = TodoStore.find(id), todo = _todos[idx];
+
+      if (todo) {
+        $.ajax({
+          method: 'DELETE',
+          url: 'api/todos/' + id,
+          success: function() {
+            _todos.splice(idx, 1);
+            that.changed();
+          }
+        });
+      }
+    },
+
+    all: function(){
+      return _todos.slice();
+    },
+
+    toggleDone: function(id) {
+      var that = this;
+      var todo = _todos[TodoStore.find(id)];
+      var done = !todo.done;
+
+      if (todo) {
+        $.ajax({
+          method: 'PATCH',
+          url: 'api/todos/' + id,
+          data: { todo: {done: done}},
+          success: function() {
+            todo.done = done;
+            that.changed();
+          }
+        });
+      }
+    }
+  };
+})(this);
